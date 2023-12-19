371b03abcbb65030d8f7c2bbc5407b0a4a40534f
Ken Stevens <141131905+Kenarco@users.noreply.github.com>
2023-12-19 13:53:27 +0800

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+export const SITE_NAME = "Ken's blog";
+export const SITE_TITLE = "Ken's Blog | A personal blog by Ken Stevens";
+export const SITE_DESCRIPTION =
+  "Ken's Blog | A personal blog by Ken's Blog";
+export const TWITTER_HANDLE = "https://twitter.com/kens761";
+export const GITHUB_HANDLE = "https://github.com/kenarcov";
+export const CLOUDINARY_NAME = "example";
